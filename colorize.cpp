36f49897b8b2aa53292e80ac04c35c601bf75c2f
AbdElrahman Eid <abdelrahman.eid97@gmail.com>
2022-05-28 01:46:41 +0200

    initial commit

---
+++ colorize.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+void gotoxy(int column, int line)
+{
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(
+        GetStdHandle( STD_OUTPUT_HANDLE ),
+        coord
+    );
+}
+
+void setColor(char* textType)
+{
+    HANDLE console_color;
+    console_color = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    int colorID;
+    int i = 0;
+    switch(tolower(textType[i]))
+    {
+    case 'l':
+        i++;
+        switch(tolower(textType[i]))
+        {
+        case 'b':
+            colorID = 1;
+            break;
+        case 'g':
+            colorID = 2;
+            break;
+        case 'c':
+            colorID = 3;
+            break;
+        case 'r':
+            colorID = 4;
+            break;
+        case 'p':
+            colorID = 5;
+            break;
+        case 'y':
+            colorID = 6;
+            break;
+        case 'w':
+            colorID = 7;
+            break;
+        }
+        break;
+    case 'b':
+        colorID = 9;
+        break;
+    case 'g':
+        colorID = 10;
+        break;
+    case 'c':
+        colorID = 11;
+        break;
+    case 'r':
+        colorID = 12;
+        break;
+    case 'p':
+        colorID = 13;
+        break;
+    case 'y':
+        colorID = 14;
+        break;
+    case 'w':
+        colorID = 15;
+        break;
+    }
+
+    SetConsoleTextAttribute(console_color, colorID);
+
+}
+
+void setColor(char* textType, int column, int line)
+{
+    HANDLE console_color;
+    console_color = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    int colorID;
+    int i = 0;
+    switch(tolower(textType[i]))
+    {
+    case 'l':
+        i++;
+        switch(tolower(textType[i]))
+        {
+        case 'b':
+            colorID = 1;
+            break;
+        case 'g':
+            colorID = 2;
+            break;
+        case 'c':
+            colorID = 3;
+            break;
+        case 'r':
+            colorID = 4;
+            break;
+        case 'p':
+            colorID = 5;
+            break;
+        case 'y':
+            colorID = 6;
+            break;
+        case 'w':
+            colorID = 7;
+            break;
+        }
+        break;
+    case 'b':
+        colorID = 9;
+        break;
+    case 'g':
+        colorID = 10;
+        break;
+    case 'c':
+        colorID = 11;
+        break;
+    case 'r':
+        colorID = 12;
+        break;
+    case 'p':
+        colorID = 13;
+        break;
+    case 'y':
+        colorID = 14;
+        break;
+    case 'w':
+        colorID = 15;
+        break;
+    }
+
+    gotoxy(column, line);
+    SetConsoleTextAttribute(console_color, colorID);
+
+}
---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdlib.h>
+#include "colorize.h"
+
+using namespace std;
+
+int main()
+{
+    setColor("LB");
+    cout << " -> LB Colorize!" << endl;
+
+    setColor("LG");
+    cout << " -> LG Colorize!" << endl;
+
+    setColor("LC");
+    cout << " -> LC Colorize!" << endl;
+
+    setColor("LR");
+    cout << " -> LR Colorize!" << endl;
+
+    setColor("LP");
+    cout << " -> LP Colorize!" << endl;
+
+    setColor("LY");
+    cout << " -> LY Colorize!" << endl;
+
+    setColor("LW");
+    cout << " -> LW Colorize!" << endl;
+
+    setColor("B");
+    cout << " -> B Colorize!" << endl;
+
+    setColor("G");
+    cout << " -> G Colorize!" << endl;
+
+    setColor("C");
+    cout << " -> C Colorize!" << endl;
+
+    setColor("R");
+    cout << " -> R Colorize!" << endl;
+
+    setColor("P");
+    cout << " -> P Colorize!" << endl;
+
+    setColor("Y");
+    cout << " -> Y Colorize!" << endl;
+
+    setColor("W");
+    cout << " -> W Colorize!" << endl;
+
+    return 0;
+}
